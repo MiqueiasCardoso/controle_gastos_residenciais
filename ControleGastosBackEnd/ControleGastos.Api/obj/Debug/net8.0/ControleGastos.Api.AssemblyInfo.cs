e798103784b3a3ebada614cd39bedebb90d5864b
unknown <miiiq.pva@hotmail.com>
2026-03-21 22:05:08 -0400

    ajustes no banco sqlite

--- ControleGastosBackEnd/ControleGastos.Api/obj/Debug/net8.0/ControleGastos.Api.AssemblyInfo.cs
+++ ControleGastosBackEnd/ControleGastos.Api/obj/Debug/net8.0/ControleGastos.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControleGastos.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6243a7eb166c30e6f70b83ae295ddec86d185ff7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72cd6b6aa06326c0a1fdc97d84bfe7cac7b13bed")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControleGastos.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControleGastos.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
